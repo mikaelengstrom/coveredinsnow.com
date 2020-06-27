306d166aa7e97ad1237188a1d83b94c78021a373
mien <mikael.pj.engstrom@gmail.com>
2020-06-27 23:00:32 +0200

    Make tap-targets accessible on mobile

--- src/components/ArrowRight.js
+++ src/components/ArrowRight.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const ArrowRight = () => {
   return (
-      <div className={'arrow-right'} style={{display: 'inline-block', position: 'relative', top:5, left: 5}}>
+      <div className={'arrow-right'} style={{display: 'inline-block', position: 'relative', left: 8}}>
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
       </div>
   )
